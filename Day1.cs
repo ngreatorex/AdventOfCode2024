a6c86a2a9bdb37be05237b0b6eec9ba89759f258
Neil Greatorex <neil@greatorex.me.uk>
2024-12-01 21:39:38 +0000

    Convert to top level statements

--- Day1.cs
+++ Day1.cs
@@ -2,35 +2,29 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 
-public class Day1 
+var lines = File.ReadLinesAsync("Day1.txt");  
+var count = 0;
+var listOne = new List<int>();
+var listTwo = new List<int>();
+
+await foreach (var line in lines)
 {
-    public static async Task Main() 
-    {
-        var lines = File.ReadLinesAsync("Day1.txt");  
-        var count = 0;
-        var listOne = new List<int>();
-        var listTwo = new List<int>();
+	var nums = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse).ToArray();
+	if (nums.Length != 2)
+		continue;
 
-        await foreach (var line in lines)
-        {
-            var nums = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse).ToArray();
-            if (nums.Length != 2)
-                continue;
-
-            listOne.Add(nums[0]);
-            listTwo.Add(nums[1]);
-            count++;
-        }
-
-	Console.WriteLine($"Number of valid lines: {count}");
-
-        listOne.Sort();
-        listTwo.Sort();
-
-        var sum = listOne.Zip(listTwo, (a, b) => Math.Abs(b - a)).Sum();
-        Console.WriteLine($"Sum of differences: {sum}");
-
-        var similarityScore = listOne.Select(a => listTwo.Count(b => b == a) * a).Sum();
-        Console.WriteLine($"Similarity score: {similarityScore}");
-    }
+	listOne.Add(nums[0]);
+	listTwo.Add(nums[1]);
+	count++;
 }
+
+Console.WriteLine($"Number of valid lines: {count}");
+
+listOne.Sort();
+listTwo.Sort();
+
+var sum = listOne.Zip(listTwo, (a, b) => Math.Abs(b - a)).Sum();
+Console.WriteLine($"Sum of differences: {sum}");
+
+var similarityScore = listOne.Select(a => listTwo.Count(b => b == a) * a).Sum();
+Console.WriteLine($"Similarity score: {similarityScore}");
